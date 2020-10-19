391877283a7c726971697d428ba7aec95a89bd35
Gabrigoo <gabrigoo@gmail.com>
2020-10-19 13:19:43 +0800

    Updates

---
+++ precache-manifest.fec5167011991f29865891c5c1919266.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9095170583b1fb19b633f75febddbe5b",
+    "url": "/hanzi-learner/index.html"
+  },
+  {
+    "revision": "353708028e681a63f09b",
+    "url": "/hanzi-learner/static/css/main.7ab81003.chunk.css"
+  },
+  {
+    "revision": "6f4d67b6397566f26a8c",
+    "url": "/hanzi-learner/static/js/2.c5d32922.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/hanzi-learner/static/js/2.c5d32922.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "353708028e681a63f09b",
+    "url": "/hanzi-learner/static/js/main.07bbb73a.chunk.js"
+  },
+  {
+    "revision": "0bfa4dd7704828d01a49",
+    "url": "/hanzi-learner/static/js/runtime-main.00919829.js"
+  }
+]);
